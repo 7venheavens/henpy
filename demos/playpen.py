32c7c9f660a74e9e90db9f32ac8a95e02d6c1121
7venheavens <7venheavens@gmail.com>
2019-03-08 21:38:39 +0800

    Functional Playpen

--- demos/playpen.py
+++ demos/playpen.py
@@ -1,17 +1,22 @@
-from henpy.searchers import searchers
-from henpy.utilities.tagtools import TagManager
+from henpy import searchers
+from henpy.utilities.tagtools import SQLTagManager
+from henpy.misc import cached_pages_dir
+import logging
 
+logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger("chardet.charsetprober").setLevel(logging.INFO)
 
 # tm = TagManager.from_file("data/tagdata.tsv")
-tm = TagManager()
-s = searchers.JavlibrarySearcher(tm)
+tm = SQLTagManager("sqlite://")
+s = searchers.JavlibrarySearcher(debug=cached_pages_dir)
 
 a = s.search("LOVE-049", return_multi=True)
 print(a[0].tags)
 print(a)
 for i in a:
-    print(i)
+    print(i, i.tags)
 
-a = s.search("LOVE-001", return_multi=True)
+a = s.search("LOVE-001", return_multi=True, topn=4, guess_with_code=False)
+print(a)
 for i in a:
     print(i)
