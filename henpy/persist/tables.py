ff29e60db1e3985284108a8207131a2b3d54bb44
7venheavens <7venheavens@gmail.com>
2019-03-24 19:33:05 +0800

    Unique constraint for tagdata, additional fixes and refactors

--- henpy/persist/tables.py
+++ henpy/persist/tables.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, ForeignKey, Integer, String, Date, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
-
+from sqlalchemy.schema import UniqueConstraint
 
 Base = declarative_base()
 
@@ -30,23 +30,41 @@ class Tag(Base):
         secondary=video_tag,
         back_populates="tags")
 
+    def __str__(self):
+        return f"<Tag: name={self.name}>"
+
+    def __repr__(self):
+        return self.__str__()
+
 
 class TagData(Base):
     """Language specific tag data
-    @attrs
-        id
-        tag
-        language
-        name
-        display_name
+
+    Attributes:
+        display_name (TYPE): Name to be displayed
+        id (TYPE): Description
+        language (TYPE): Description
+        name (TYPE): Core unified name
+        tag_id (TYPE): ID of the tag
+        type (TYPE): Description
+        source_id (string): Id of the tag from the source
     """
     __tablename__ = "tag_data"
     id = Column(Integer, primary_key=True)
     tag_id = Column(Integer, ForeignKey("tag.id"))
-    type = Column(String(255))
+    category = Column(String(255), nullable=True)
     language = Column(String(20), nullable=False)
     name = Column(String(255), nullable=False, index=True)
     display_name = Column(String(255), nullable=False)
+    source_id = Column(String(255), nullable=True, index=True)
+
+    __table_args__ = (UniqueConstraint("source_id", "language", name="_source_id_language"),
+                      )
+
+    tag = relationship("Tag")
+
+    def __repr__(self):
+        return f"<TagData: name={self.name}, tag={self.tag}, source_id={self.source_id}>"
 
 
 class Video(Base):
@@ -56,7 +74,7 @@ class Video(Base):
 
     id = Column(Integer, primary_key=True)
     code = Column(String(50), nullable=False, index=True)
-    release_date = Column(Date, index=True)
+    release_date = Column(Date, index=True, nullable=True)
     image_path = Column(String(255), nullable=True)  # Images are optional
     director = Column(String(100), nullable=False)
     maker = Column(String(100), nullable=False)
